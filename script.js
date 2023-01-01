07f969343de9ab6feb9498ea83bf16a6ee5b90c1
Kanni <magnificentzeem@gmail.com>
2023-01-01 12:56:18 +0100

    complete the logic for a game of one round

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 function getComputerChoice() {
-    choices = ['Rock', 'Paper', 'Scissors'];
+    choices = ['rock', 'paper', 'scissors'];
     return (choices[Math.floor(Math.random() * choices.length)])
 }
 
-function play(player, computer) {
-    return ()
+function getPlayerChoice() {
+    let input = prompt("Paper, rock or scissors").toLowerCase()
+
+    if (input === 'rock' || input === 'paper' || input === 'scissors') {
+        return input;
+    }
+    else {
+        console.error("Invalid input: Enter 'rock', 'paper', or 'scissors'")
+    }
 }
+
+function play(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        result = `it's a tie!!, you both played ${playerSelection}`;
+    }
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+         (playerSelection === 'paper' && computerSelection === 'rock') || 
+         (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            result = `You Win! ${playerSelection} beats ${computerSelection}`
+         }
+    else {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+    return (result)
+}
+
+play(getPlayerChoice(), getComputerChoice())
